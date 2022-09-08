ada15f538df273194bbb3b6eddbc8bb7112839aa
Tina <tinav1409@gmail.com>
2022-09-08 23:30:04 +0530

    d to b ; b to d

---
+++ binary & decimal number system.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+/*
+//decimal to binary
+	int n;
+	cout<<"Enter the value of n: "<<endl;
+	cin>>n;
+	
+	int ans=0;
+	int i=0;
+	
+	while(n!=0){
+		
+		int bit= n&1;
+		
+		ans=(bit * pow(10,i)) + ans;
+	
+		n=n>>1;
+		i++;
+	}
+	cout<<"The answer in Binary is : "<<ans<<endl;
+*/
+/*
+//binary to decimal
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    
+    int ans=0;
+    int i=0;
+    
+    while(n!=0){
+    	
+    	int decimal = n%10;
+    	
+    	if (decimal ==1){
+    		ans=ans+pow(2,i);
+    	}
+		n=n/10;
+		i++;
+	}
+	cout<<"the answer in decimal is: "<<ans<<endl;
+*/	
+	
+}
+
