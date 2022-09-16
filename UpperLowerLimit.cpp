5c21ff99aac548a985da8949bbe7832f19372a44
Tina <tinav1409@gmail.com>
2022-09-16 20:19:36 +0530

    pracice

---
+++ UpperLowerLimit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout<<"The lower limit of int is:  "<<INT_MIN<<endl;   //int
+	cout<<"The upper limit of int is:  "<<INT_MAX<<endl;
+	cout<<"The lower limit of char is: "<<CHAR_MIN<<endl; //char
+	cout<<"The upper limit of char is: "<<CHAR_MAX<<endl;
+	cout<<"The upper limit of uint is: "<<UINT_MAX<<endl; //unsigned int
+
+}
