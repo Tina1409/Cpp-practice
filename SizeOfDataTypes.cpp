bcc58cd337b956f6a34ca5948ed2a3c519c4081c
Tina <tinav1409@gmail.com>
2022-09-19 22:33:43 +0530

    checking data type size

--- SizeOfDataTypes.cpp
+++ SizeOfDataTypes.cpp
@@ -7,5 +7,6 @@ int main(){
 	cout<<"The size of char is:   "<<sizeof(char)<<endl;
 	cout<<"The size of double is: "<<sizeof(double)<<endl;
 	cout<<"The size of bool is:   "<<sizeof(bool)<<endl;
-	
+	cout<<"The size of long is:   "<<sizeof(long)<<endl;
+	cout<<"The size of long long is:   "<<sizeof(long long)<<endl;	
 }
