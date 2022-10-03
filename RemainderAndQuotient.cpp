30abf5c8bc8570e56e6adcf719a3be1b8c10e3c8
Tina <tinav1409@gmail.com>
2022-10-03 22:58:21 +0530

    findingRemainderAndQuotient

---
+++ RemainderAndQuotient.cpp
@@ -0,0 +1,19 @@
+//printing remainder and quotient
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	int b;
+	float quotient;
+	int remainder;
+	cout<<"Enter the value of a"<<endl;
+	cin>>a;
+	cout<<"Enter the value of b"<<endl;
+	cin>>b;
+	quotient=a/b;
+	remainder=a%b;
+	cout<<"The remainder is "<<remainder<<endl;
+	cout<<"The quotient is "<<quotient<<endl;
+	
+}
+
